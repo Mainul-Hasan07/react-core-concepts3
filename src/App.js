b38fd39b6157a72a6b67e720a035a9b57982fc62
unknown <mainulhasansadik@gmail.com>
2021-09-19 01:20:54 +0600

    React Core Concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadComments></LoadComments>
     </div>
   );
+}
+
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data=>setComments(data))
+  },[])
+  return (
+    <div>
+      <h3>Load Comments</h3>
+      {
+        comments.map(comment=> <Comment name={Comment.name} email={comment.email}></Comment>)
+      }
+    </div>
+  )
+}
+
+function Comment(props) {
+  return (
+    <div style={{backgroundColor:'skyblue',border:'3px solid blue'}}>
+      <h3>Name: {props.name}</h3>
+      <h3>Email: {props.email}</h3>
+    </div>
+  )
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleReset = () => setCount(0);
+  return (
+    <div>
+      <h2>count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      
+    </div>
+  )
 }
 
 export default App;
